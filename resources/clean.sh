97e7695d485c5ac7699100e6992b7b2bf93bace3
Robert Poenaru <robert.poenaru@protonmail.ch>
2022-01-02 17:15:14 +0200

    update default abstract

--- resources/clean.sh
+++ resources/clean.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-rm *.aux
-rm *.fdb_latexmk
-rm *.log
-rm *.lyx~
-rm *.synctex.gz
-rm *.fls
+rm *.aux > /dev/null 2>&1
+rm *.fdb_latexmk > /dev/null 2>&1
+rm *.log > /dev/null 2>&1
+rm *.lyx~ > /dev/null 2>&1
+rm *.synctex.gz > /dev/null 2>&1
+rm *.fls > /dev/null 2>&1
