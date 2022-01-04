6dffe9644f20f15a457fa48f33bab21796749c20
Robert Poenaru <robert.poenaru@protonmail.ch>
2022-01-04 04:36:28 +0200

    Update clean.sh

--- resources/clean.sh
+++ resources/clean.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+# source for avoiding extra messages on failed shell command
+# https://stackoverflow.com/questions/31318068/shell-script-to-remove-a-file-if-it-already-exist/31318262
 rm *.aux > /dev/null 2>&1
 rm *.fdb_latexmk > /dev/null 2>&1
 rm *.log > /dev/null 2>&1
