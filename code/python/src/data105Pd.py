cc12c7aa4f21f00fcf868269e755970e3afa5eb9
Robert Poenaru <robert.poenaru@protonmail.ch>
2022-02-22 12:37:35 +0200

    add tools for extracting energies

--- code/python/src/data105Pd.py
+++ code/python/src/data105Pd.py
@@ -6,6 +6,9 @@
 
 
 class Tools:
+    """
+    - tools for extracting and parsing the experimental data
+    """
     @staticmethod
     def ExtractSpins(raw_string):
         # separate all the spins by the commmas
@@ -14,7 +17,37 @@ class Tools:
         # with the integer part determined, compute the numerical values of each spins
         spins = [float(x[:-2]) / 2 for x in extracted_spins]
 
+        # perform sorting of the spins in ascending order
+        spins.sort()
+
         return spins
+
+    @staticmethod
+    def keV_to_MeV(energies):
+        """
+        - transform the energies from units of keV to units of MeV
+        - 1 MeV is 1000 keV
+        """
+
+        energies_mev = [float(x / 1000) for x in energies]
+
+        # also sort the energies within ascending order
+        energies_mev.sort()
+
+        return energies_mev
+
+    @staticmethod
+    def normalize_energies(energies, band_head):
+        """
+        - subtract the value of the band-head (first energy within the YRAST band) from all the energies 
+        """
+
+        normalized_energies = [float(x - band_head) for x in energies]
+
+        # sort the energies in ascending order
+        normalized_energies.sort()
+
+        return normalized_energies
 
 
 class Band_B:
@@ -24,6 +57,9 @@ class Band_B:
     - signature: \alpha=+1/2
     - negative parity spin states: \pi=-1
     """
+
+    SIGNATURE = +0.5
+    PARITY = -1
 
     band_B_energies = [8405, 7190, 6071, 4952, 3800, 2700, 1742, 970, 489]
     """ the absolute energies """
@@ -41,6 +77,9 @@ class Band_A:
     - negative parity spin states: \pi=-1
     """
 
+    SIGNATURE = -0.5
+    PARITY = -1
+
     band_A_energies = [8405, 7190, 6071, 4952, 3800, 2700, 1742, 970, 489]
     """ the absolute energies """
     band_A_spins = '43/2,39/2,35/2,31/2,27/2,23/2,19/2,15/2,11/2'
@@ -53,9 +92,12 @@ class Band_C:
     """
     - the band C - SIGNATURE PARTNER of band A (according to Timar et al 2019)
     - similar parity as the Band A
-    - built on the neutron h=11/2 configuration 
+    - built on the neutron h=11/2 configuration
     - opposite signature as of Band A: \alpha=+1/2
     """
+
+    SIGNATURE = +0.5
+    PARITY = -1
 
     band_C_spins = '29/2,25/2,21/2'
     band_C_energies = [4955, 3859, 2900]
@@ -70,6 +112,11 @@ def main():
     print(a_band_spins)
     print(b_band_spins)
     print(c_band_spins)
+
+    a_band_energies = Tools.keV_to_MeV(Band_A.band_A_energies)
+
+    print(Band_A.band_A_energies)
+    print(a_band_energies)
 
 
 if __name__ == '__main__':
