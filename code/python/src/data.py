ebc31a2b382de5d64fdfbf38d7ea5ee78bf7944b
Robert Poenaru <robert.poenaru@protonmail.ch>
2022-02-22 09:59:23 +0200

    add experimental data for signature partner band

--- code/python/src/data.py
+++ code/python/src/data.py
@@ -9,6 +9,6 @@ band_A_spins = '43/2,39/2,35/2,31/2,27/2,23/2,19/2,15/2,11/2'
 band_A_energies = [8405, 7190, 6071, 4952, 3800, 2700, 1742, 970, 489]
 band_A_transitions = [1215, 1119, 1119, 1152, 1100, 958, 481]
 
-band_SP_spins = ''
-band_SP_energies = []
-band_SP_transitions = []
+band_SP_spins = '29/2,25/2,21/2'
+band_SP_energies = [4955, 3859, 2900]
+band_SP_transitions = [1097, 959, 939]
